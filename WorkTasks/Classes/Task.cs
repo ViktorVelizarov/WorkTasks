efe860051de6429ad4ba0cb746ef056d4f9afce0
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-12-16 18:38:19 +0100

    create base classes and enums

---
+++ WorkTasks/Classes/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTasks.Classes
+{
+    public class Task
+    {
+        //fields
+        private string title;
+        private List<DepartmentsEnum> departments;
+        private StatusEnum status;
+        private string description;
+        private Employee employee;
+
+        //constructor
+        public Task(string title, string description) 
+        {
+            this.title = title;
+            this.description = description;
+        }  
+    }
+}
